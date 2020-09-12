e0c28b3be0aee31ae2b137b8afe47e1e11281dd6
Putta Ganesh <54807149+ganesh8074@users.noreply.github.com>
2020-09-12 11:49:09 +0530

    Create Intro to Tutorial Challenges.cpp

---
+++ Intro to Tutorial Challenges.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,v,n,index=0;
+cin>>v>>n;
+
+for(int i=0;i<n;i++)
+    {
+    cin>>a;
+    if(a==v)
+        {
+        cout<<index<<endl;
+        }
+    index++;
+    }
+return 0;
+}
