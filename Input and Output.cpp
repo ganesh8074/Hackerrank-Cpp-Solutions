cbf59cf616d1b63ac7306cc5ee719d6e0704a8ee
Putta Ganesh <54807149+ganesh8074@users.noreply.github.com>
2020-09-12 11:47:55 +0530

    Create Input and Output.cpp

---
+++ Input and Output.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+
+int a,b,c;
+void input()
+{
+cin>>a>>b>>c;
+}
+
+int sum()
+{
+cout<<a+b+c;
+return 0;
+}
+
+int main()
+{
+input();
+sum();
+return 0;
+}
